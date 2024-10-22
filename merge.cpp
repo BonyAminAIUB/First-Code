290085f00f10a242050d1431469f1854bc316135
Bony Amin <bonyamincse@gmail.com>
2024-10-22 10:12:25 +0600

    cpp file

---
+++ merge.cpp
@@ -0,0 +1,12 @@
+// Take two strings as input and merge them
+#include <iostream>
+using namespace std;
+int main(){
+    string str1,str2;
+    cout << "Enter sample input 1: ";
+    getline(cin,str1);
+    cout  << "Enter sample input 2: ";
+    getline(cin,str2);
+    cout << "Sample output: " << str1 << " " << str2 << endl;
+    return 0;
+}
