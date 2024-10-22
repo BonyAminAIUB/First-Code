3f041940060390549261753f698c509689230e8f
Bony Amin <bonyamincse@gmail.com>
2024-10-22 09:16:21 +0600

    cpp file

---
+++ lengthDS.cpp
@@ -0,0 +1,19 @@
+// Take a string as input and count its length
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string str;
+    cout << "Enter sample input: ";
+    getline(cin, str);
+    
+    int i;
+    for( i = 0; str[i] != '\0' ; i++){
+        
+    }
+    cout << "Length of string is " << i<< endl;
+   
+    return 0;
+}
