5f3d14f1afcc645b2743d20406531fd39df4733c
Bony Amin <bonyamincse@gmail.com>
2024-11-04 19:43:44 +0600

    cpp file

--- first.cpp
+++ first.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-namespace test{
-    int myNumber = 10;
-}
-int main(){
-    int myNumber = 100;
-    cout << myNumber << endl;
-    cout << test:: myNumber << endl;
-    return 0;
+int main()
+{
+
+    int a[5][5]={{1,2,3,4,5},
+                 {6,7,8,9,10},
+                 {1,2,3,4,5},
+                 {6,7,8,9,10},
+                 {2,2,3,4,5}};
+    int PrimaryDiagonalSum =0;
+    int SecondaryDiagonalSum = 0;
+    for(int i=0;i<5;i++)
+    {
+       PrimaryDiagonalSum+= a[i] [i];
+       SecondaryDiagonalSum+=a[4-i][i];
+
+    }
+    /*for(int i=0;i<5;i++)
+    {
+        SecondaryDiagonalSum+=a[i][4-i];
+    }*/
+    cout<<"Primary Diagonal Sum : " <<PrimaryDiagonalSum<<endl;
+    cout<<"Secondary Diagonal sum : " <<SecondaryDiagonalSum<<endl;
 }
